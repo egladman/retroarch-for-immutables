c1d2c9cbde337f8c184e2c4edb86872ee4b043e3
Eli Gladman <8713153+egladman@users.noreply.github.com>
2023-06-11 02:15:28 -0400

    fix edgecase where uid isnt mapped to a name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,42 @@
 #!/usr/bin/env bash
 
-base_dir=/app
+set -o errexit
 
-set -o allexport -o errexit
-XDG_CONFIG_HOME="$base_dir"
-XDG_STATE_HOME="$base_dir"
-XDG_DATA_HOME="$base_dir"
-XDG_RUNTIME_DIR=/tmp
-unset base_dir
-set +o allexport # Disable
+res="$(id -nu)"
+regex='^[0-9]+$'
+base_dir="/dev/shm/${res}"
+if [[ ! "$res" =~ $regex ]] && [[ "$res" != "root" ]]; then
+    base_dir="/home/${res}"
+fi
 
-eval "$(rafi init bash)"
+xdg_config_home_fallback="${base_dir}/.config"
+xdg_state_home_fallback="${base_dir}/.local/state"
+xdg_data_home_fallback="${base_dir}/.local/share"
 
+set -o allexport
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$xdg_config_home_fallback}"
+XDG_STATE_HOME="${XDG_STATE_HOME:-$xdg_state_home_fallback}"
+XDG_DATA_HOME="${XDG_DATA_HOME:-$xdg_data_home_fallback}"
+XDG_RUNTIME_DIR="/tmp"
+
+if [[ "$PATH" != "/app/rafi/rootfs"* ]]; then
+    printf '%s\n' "Modifying PATH"
+    PATH="/app/rafi/rootfs/bin:${PATH}"
+fi
+
+if [[ "$LD_LIBRARY_PATH" != "/app/rafi/rootfs"* ]]; then
+    printf '%s\n' "Modifying LD_LIBRARY_PATH"
+    LD_LIBRARY_PATH="/app/rafi/rootfs/lib64:/app/rafi/rootfs/lib"
+fi
+set +o allexport
+
+for d in "$XDG_CONFIG_HOME" "$XDG_STATE_HOME" "$XDG_DATA_HOME" "$XDG_RUNTIME_DIR"; do
+    if [[ -d "$d" ]]; then
+	continue
+    fi
+    printf '%s\n' "Creating directory: $d"
+    mkdir -p "$d"
+done
+
+printf '%s\n' "Entrypoint complete. Executing: $*"
 exec "$@"
